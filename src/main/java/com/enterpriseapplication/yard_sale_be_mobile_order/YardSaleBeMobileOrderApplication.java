42040d0fc581ecc67f4106f80060015bec1142f5
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-11-15 18:07:13 +0100

    First Commit.

---
+++ src/main/java/com/enterpriseapplication/yard_sale_be_mobile_order/YardSaleBeMobileOrderApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseapplication.yard_sale_be_mobile_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardSaleBeMobileOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardSaleBeMobileOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterpriseapplication/yard_sale_be_mobile_order/YardSaleBeMobileOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterpriseapplication.yard_sale_be_mobile_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardSaleBeMobileOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
